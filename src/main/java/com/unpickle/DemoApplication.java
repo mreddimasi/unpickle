9196127f3caeb31301dd467b5fbaf116ef680895
Madhukar <mreddimasi@gmail.com>
2016-03-02 18:00:14 +0530

    Elastic Files addition

--- src/main/java/com/unpickle/DemoApplication.java
+++ src/main/java/com/unpickle/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class DemoApplication {
 
-    @RequestMapping("/")
+    @RequestMapping("/demo")
     public String index() {
         return "Greetings from Spring Boot!";
     }
