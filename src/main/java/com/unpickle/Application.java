755e4ddf333d6a6039b6716ca762bd8cf3e11a48
Madhukar <mreddimasi@gmail.com>
2016-03-02 20:20:04 +0530

    Add Main class for ratpack

--- src/main/java/com/unpickle/Application.java
+++ src/main/java/com/unpickle/Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         System.out.println("Let's inspect the beans provided by Spring Boot:");
@@ -19,6 +19,6 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
-    }
+    }*/
 
 }
